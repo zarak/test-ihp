f781acb9f4d028a1a6d06c33b359975f8f70ec19
Zarak <7254237+zarak@users.noreply.github.com>
2021-01-02 17:31:05 +0500

    Revert "Add smtp-mail package"
    This reverts commit f838febfbdb9ee2ad3ef4921ab7eb26f53df8b1a.

--- default.nix
+++ default.nix
@@ -13,7 +13,6 @@ let
             hlint
             p.ihp
             mmark
-            smtp-mail
         ];
         otherDeps = p: with p; [
             # Native dependencies, e.g. imagemagick
