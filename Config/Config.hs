d3e1a94946b5daf8631a19d3f9dfbb65cbf34272
Zarak <7254237+zarak@users.noreply.github.com>
2021-01-03 10:20:40 +0500

    Add tailwind override

--- Config/Config.hs
+++ Config/Config.hs
@@ -4,9 +4,16 @@ import IHP.Prelude
 import IHP.Environment
 import IHP.FrameworkConfig
 import IHP.View.CSSFramework
+import IHP.HtmlSupport.QQ (hsx)
+import IHP.FlashMessages.Types
+
+customCSSFramework =
+    tailwind { styledFlashMessage } 
+        where styledFlashMessage _ (SuccessFlashMessage message) = [hsx|<div class="alert alert-success">{message}</div>|]
+
 
 config :: ConfigBuilder
 config = do
     option Development
     option (AppHostname "localhost")
-    option tailwind
+    option customCSSFramework
